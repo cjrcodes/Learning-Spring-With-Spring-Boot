0e6764b51e7940f5e5573dd13387b836117b5fd7
Christian <cjrcodes@gmail.com>
2022-06-01 18:46:30 -0400

    Chapter 3 Completed

--- src/main/java/com/cjrcodes/lil/learningspring/util/ApplicationStartup.java
+++ src/main/java/com/cjrcodes/lil/learningspring/util/ApplicationStartup.java
@@ -5,6 +5,10 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import com.cjrcodes.lil.learningspring.data.Guest;
+import com.cjrcodes.lil.learningspring.data.GuestRepository;
+import com.cjrcodes.lil.learningspring.data.Reservation;
+import com.cjrcodes.lil.learningspring.data.ReservationRepository;
 import com.cjrcodes.lil.learningspring.data.Room;
 import com.cjrcodes.lil.learningspring.data.RoomRepository;
 
@@ -12,9 +16,14 @@ import com.cjrcodes.lil.learningspring.data.RoomRepository;
 public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent>{
 
 	private final RoomRepository roomRepository;
+	private final GuestRepository guestRepository;
+	private final ReservationRepository reservationRepository;
+
 	
-	public ApplicationStartup(RoomRepository roomRepository) {
+	public ApplicationStartup(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
 		this.roomRepository = roomRepository;
+		this.guestRepository = guestRepository;
+		this.reservationRepository = reservationRepository;
 	}
 
 	@Override
@@ -22,7 +31,10 @@ public class ApplicationStartup implements ApplicationListener<ApplicationReadyE
 		System.out.println("App Started");
 		Iterable<Room> rooms = this.roomRepository.findAll();
 		rooms.forEach(System.out::println);
-
+		Iterable<Guest> guests = this.guestRepository.findAll();
+		guests.forEach(System.out::println);
+		Iterable<Reservation> reservations = this.reservationRepository.findAll();
+		reservations.forEach(System.out::println);
 	}
 	
 	
